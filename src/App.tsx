32c3f4c411d01fa80672fc48336f3239c78ceff9
abhitoshanand <110685657+abhitoshanand@users.noreply.github.com>
2024-11-16 19:30:25 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,6 @@ import { About } from './components/About';
 import { Education } from './components/Education';
 import { Experience } from './components/Experience';
 import { Projects } from './components/Projects';
-import { VideoLectures } from './components/VideoLectures';
 import { Contact } from './components/Contact';
 import { Footer } from './components/Footer';
 
@@ -20,7 +19,6 @@ export function App() {
           <Education />
           <Experience />
           <Projects />
-          <VideoLectures />
           <Contact />
         </main>
         <Footer />
